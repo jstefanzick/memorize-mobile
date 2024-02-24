b4908e061ddabd66f8e6ee09a0dcd292892e8f7f
jstefanzick <stefanzi@seattleu.edu>
2024-02-24 10:28:19 -0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Julian Stefanzick on 2/24/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
