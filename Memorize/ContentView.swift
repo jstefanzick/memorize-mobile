8e8d88f53f954236eb9a7cff674784a5783580fd
jstefanzick <stefanzi@seattleu.edu>
2024-02-24 13:42:45 -0800

    Basic Card

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -7,17 +7,20 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+        ZStack(content: {
+            RoundedRectangle(cornerRadius: 12).foregroundColor(.white)
+            RoundedRectangle(cornerRadius: 12).strokeBorder(lineWidth: 2)
+            Text("👻").font(.largeTitle)
+        })
+        .foregroundColor(.orange)
+        .imageScale(.small)
         .padding()
     }
 }
+
 
 #Preview {
     ContentView()
