748d9f9f2f62908a77d6fefe61a938616ff7a61c
jstefanzick <stefanzi@seattleu.edu>
2024-02-24 15:32:21 -0800

    Updated add/remove card buttons Placed card creations sections/buttons into separate vars

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -1,9 +1,5 @@
-//
-//  ContentView.swift
 //  Memorize
-//
-//  Created by Julian Stefanzick on 2/24/24.
-//
+//  Created by Julian Stefanzick on 2/24/24
 
 import SwiftUI
 
@@ -16,24 +12,53 @@ struct ContentView: View {
     
     var body: some View {
         VStack{
-            HStack {
-                ForEach(0..<cardCount, id: \.self) { index in
-                    CardView(content: emojis[index])
-                }
-            }.foregroundColor(.orange)
-            HStack{
-                Button("Remove Card"){
-                    cardCount -= 1
-                }
-                Spacer()
-                Button("Add Card"){
-                    cardCount += 1
-                }
-            }.padding()
+            cards
+            cardAdjusters
         }
-            .padding()
+        .padding()
+    }
+    
+    var cards: some View {
+        HStack {
+            ForEach(0..<cardCount, id: \.self) { index in
+                CardView(content: emojis[index])
+            }
+        }
+        .foregroundColor(.orange)
+    }
+    
+    var cardAdjusters: some View {
+        HStack{
+            cardRemover
+            Spacer()
+            cardAdder
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+    }
+    
+    var cardRemover: some View {
+        Button( action: {
+            if cardCount > 1 {
+                cardCount -= 1
+            }
+        },  label: {
+             Image(systemName: "rectangle.stack.badge.minus.fill")
+        })
+    }
+    
+    var cardAdder: some View {
+        Button( action: {
+            if cardCount < emojis.count {
+                cardCount += 1
+            }
+        },  label: {
+             Image(systemName: "rectangle.stack.badge.plus.fill")
+        })
     }
 }
+
+
 
 struct CardView: View {
     let content: String
