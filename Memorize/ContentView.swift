8503deba939063e32188578fc8111dd622a9875c
jstefanzick <stefanzi@seattleu.edu>
2024-02-24 14:50:48 -0800

    ForEach card emoji loader

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,19 +9,22 @@ import SwiftUI
 
 
 struct ContentView: View {
+    
+    let emojis: [String] = ["👻", "🙉", "🤔", "🐶"]
+    
     var body: some View {
-        HStack(content: {
-            CardView()
-            CardView()
-            CardView()
-            CardView()
-        })
+        HStack {
+                ForEach(emojis.indices, id: \.self) { index in
+                    CardView(content: emojis[index])
+                }
+        }
         .foregroundColor(.orange)
         .padding()
     }
 }
 
 struct CardView: View {
+    let content: String
     @State var isFaceUp = true
     
     var body: some View {
@@ -30,7 +33,7 @@ struct CardView: View {
             if isFaceUp {
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 2)
-                Text("👻").font(.largeTitle)
+                Text(content).font(.largeTitle)
             } else{
                 base.fill()
             }
