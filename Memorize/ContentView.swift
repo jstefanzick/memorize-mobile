6e746043b9755c023454639816a3374ca8c518dc
jstefanzick <stefanzi@seattleu.edu>
2024-02-25 16:50:28 -0800

    Clean up UI Create initial backend file

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -11,19 +11,15 @@ struct ContentView: View {
     @State var cardCount: Int = 4
     
     var body: some View {
-        VStack{
             ScrollView{
                 cards
             }
-            Spacer()
-            cardAdjusters
-        }
         .padding()
     }
     
     var cards: some View {
         LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
-            ForEach(0..<cardCount, id: \.self) { index in
+            ForEach(emojis.indices, id: \.self) { index in
                 CardView(content: emojis[index])
                     .aspectRatio(2/3, contentMode: .fit
                     )
@@ -32,34 +28,7 @@ struct ContentView: View {
         .foregroundColor(.orange)
     }
     
-    var cardAdjusters: some View {
-        HStack{
-            cardRemover
-            Spacer()
-            cardAdder
-        }
-        .imageScale(.large)
-        .font(.largeTitle)
-    }
-    
-    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
-        Button( action: {
-                cardCount += offset
-        },  label: {
-             Image(systemName: symbol)
-        })
-        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
-    }
-    
-    
-    var cardRemover: some View {
-        cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
-    }
-    
-    var cardAdder: some View {
-        cardCountAdjuster(by: 1, symbol: "rectangle.stack.badge.plus.fill")
-        
-    }
+
 }
 
 
---
+++ Memorize/MemorizeGame.swift
@@ -0,0 +1,8 @@
+//
+//  MemorizeGame.swift
+//  Memorize
+//
+//  Created by Julian Stefanzick on 2/25/24.
+//
+
+import Foundation
