b69f7cfbda07f9d124f9ee6759ec22b850d3c328
jstefanzick <stefanzi@seattleu.edu>
2024-02-25 16:55:47 -0800

    Model creation

--- Memorize/MemorizeGame.swift
+++ Memorize/MemorizeGame.swift
@@ -1,8 +1,22 @@
-//
 //  MemorizeGame.swift
 //  Memorize
 //
 //  Created by Julian Stefanzick on 2/25/24.
-//
 
 import Foundation
+
+struct MemorizeGame<CardContent> {
+    
+    var cards: Array<Card>
+    
+    func choose(card: Card){
+        
+    }
+    
+    struct Card {
+        var isFaceUp: Bool
+        var isMatched: Bool
+        var content: CardContent
+    }
+    
+}
